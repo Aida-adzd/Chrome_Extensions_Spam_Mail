9377dc4e6fb5351402f5a0247fff59348f20efa9
aidaz <aida.adzd@gmail.com>
2024-05-25 01:54:55 +0330

    Revert "Cleaned code"
    This reverts commit 4d6fb131c8e7d6248c175c144ba78f091f3cc138.

--- project_folder/app.py
+++ project_folder/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify, render_template, request
-from flask_cors import CORS
+from flask import Flask, render_template, request, jsonify
 from main import classify_text
+from flask_cors import CORS
 
 app = Flask(__name__, template_folder='templates')
 CORS(app)
@@ -13,13 +13,9 @@ def home():
 
 @app.route('/classify', methods=['POST'])
 def classify():
-    data = request.get_json()
-    if 'email' in data:
-        email_text = data.get('email')
-        result = classify_text(email_text)
-        return jsonify({'result': result})
-    else:
-        return jsonify({'error': 'No email text provided'}), 400
+    email_text = request.get_json().get('email')
+    result = classify_text(email_text)
+    return jsonify({'result': result})
 
 
 if __name__ == '__main__':
