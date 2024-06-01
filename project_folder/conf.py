83c5fb506821305d52735efbff7170a527abb0d7
aidaz <aida.adzd@gmail.com>
2024-06-01 20:43:40 +0330

    Refactored conf.py with more descriptive variable names and detailed comments

--- project_folder/conf.py
+++ project_folder/conf.py
@@ -1,3 +1,11 @@
-csv_file_path = r"your path"
-model_path = r"your path"
-popup_html = r"your path"
+# Path to the CSV file that contains the training data for the email classifier model
+path_to_training_data_csv = r"your path"
+
+# Path to the location where the trained email classifier model is (or will be) saved
+path_to_saved_model = r"your path"
+
+# Path to the HTML file that is used to create the popup interface for the email classifier
+path_to_popup_html = r"your path"
+
+# Replace "your path" with the actual paths to your files.
+# Ensure that the paths are correct and the files exist at the specified locations.
